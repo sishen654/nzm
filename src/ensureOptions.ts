8e23a7f356f55f313950b1f8c7081e5bf59df075
sishen <1730821420@qq.com>
2023-08-09 16:48:34 +0800

    update: add init option

--- src/ensureOptions.ts
+++ src/ensureOptions.ts
@@ -9,6 +9,7 @@ export default function ensureOptions() {
       tencent: "https://mirrors.cloud.tencent.com/npm/",
       cnpm: "https://r.cnpmjs.org/",
       taobao: "https://registry.npmmirror.com/",
+      npmMirror: "https://skimdb.npmjs.com/registry/"
     }
     writeJsonSync(NZMRC, defaultOptions)
   }
